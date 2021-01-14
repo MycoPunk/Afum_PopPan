2fcf4d069ea8a87e37d05ce15063717baa338bae
Lotus Lofgren <LotusLofgren@gmail.com>
2021-01-14 14:47:19 -0800

    Update 02_run_LD_decay.sh

--- pipelines/LD_decay/02_run_LD_decay.sh
+++ pipelines/LD_decay/02_run_LD_decay.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/bash
-#SBATCH --mem=64G -p batch --nodes 1 --ntasks 2 --out LD.log
+#SBATCH --mem=64G -p batch --nodes 1 --ntasks 2 --out logs/LD.log
+
+module load plink
+
+CPUS=$SLURM_TASKS_PER_NODE
+if [ ! $CPUS ]; then
+ CPUS=$PBS_NP
+ if [ ! $CPUS ]; then
+  CPUS=2
+ fi
+fi
+
+mkdir LD_out
 
 #Clade1
 for N in {1..50}; do \
-plink --bfile "Clade_1_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 999999 --ld-window-kb 8000 --out "Clade_1_samp${N}_LD_out"
+plink --bfile "plink_files/Clade_1_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 99999 --ld-window-kb 500 --threads $CPUS --out "LD_out/Clade_1_samp${N}_LD_out"
 done
 
 #Clade2
 for N in {1..50}; do \
-plink --bfile "Clade_2_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 999999 --ld-window-kb 8000 --out "Clade_2_samp${N}_LD_out"
+plink --bfile "plink_files/Clade_2_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 99999 --ld-window-kb 500 --threads $CPUS --out "LD_out/Clade_2_samp${N}_LD_out"
 done
 
 #Clade3
-plink --bfile "Clade_3" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 999999 --ld-window-kb 8000 --out "Clade_3_LD_out"
+for N in {1..50}; do \
+plink --bfile "plink_files/Clade_3_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 99999 --ld-window-kb 500 --threads $CPUS --out "LD_out/Clade_3_samp${N}_LD_out"
+done
 
 #clean up 
-rm *LD_out.log
-rm *LD_out.nosex
+rm LD_out/*LD_out.log
+rm LD_out/*LD_out.nosex
