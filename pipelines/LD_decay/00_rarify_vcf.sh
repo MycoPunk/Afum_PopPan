95773e4073afed951d46ab0bbf55a33154279e69
Lotus Lofgren <LotusLofgren@gmail.com>
2021-01-14 14:20:01 -0800

    Update 00_rarify_vcf.sh

--- pipelines/LD_decay/00_rarify_vcf.sh
+++ pipelines/LD_decay/00_rarify_vcf.sh
@@ -22,31 +22,31 @@ done
 
 #subset the strains using the randomly sampled strain name lists created above, from the vcf files 
 for N in {1..50}; do \
-vcftools --keep temp/C1_sample_temp${N}.txt --gzvcf vcf/pop_for_pan2_.SNP.combined_selected.NO_TEs.vcf.gz --recode --out vcf/Clade_1_samp_${N}
+vcftools --keep temp/C1_sample_temp${N}.txt --gzvcf vcf/pop_for_pan2_.SNP.combined_selected.NO_TEs.vcf.gz --recode --out temp/Clade_1_samp_${N}
 done
 
 for N in {1..50}; do \
-vcftools --keep temp/C2_sample_temp${N}.txt --gzvcf vcf/pop_for_pan2_.SNP.combined_selected.NO_TEs.vcf.gz --recode --out vcf/Clade_2_samp_${N}
+vcftools --keep temp/C2_sample_temp${N}.txt --gzvcf vcf/pop_for_pan2_.SNP.combined_selected.NO_TEs.vcf.gz --recode --out temp/Clade_2_samp_${N}
 done
 
 for N in {1..50}; do \
-vcftools --keep temp/C3_sample_temp${N}.txt --gzvcf vcf/pop_for_pan2_.SNP.combined_selected.NO_TEs.vcf.gz --recode --out vcf/Clade_3_samp_${N}
+vcftools --keep temp/C3_sample_temp${N}.txt --gzvcf vcf/pop_for_pan2_.SNP.combined_selected.NO_TEs.vcf.gz --recode --out temp/Clade_3_samp_${N}
 done
 
 #remove invariant sites using bcftools
 for N in {1..50}; do \
-bcftools view -c 1 vcf/Clade_1_samp_${N}.recode.vcf -o vcf/Clade_1_samp_${N}.recode_noinv.vcf
+bcftools view -c 1 temp/Clade_1_samp_${N}.recode.vcf -o vcf/Clade_1_samp_${N}.recode_noinv.vcf
 done
 
 for N in {1..50}; do \
-bcftools view -c 1 vcf/Clade_2_samp_${N}.recode.vcf -o vcf/Clade_2_samp_${N}.recode_noinv.vcf
+bcftools view -c 1 temp/Clade_2_samp_${N}.recode.vcf -o vcf/Clade_2_samp_${N}.recode_noinv.vcf
 done
 
 #Clade 3
 for N in {1..50}; do \
-bcftools view -c 1 vcf/Clade_3_samp_${N}.recode.vcf -o vcf/Clade_3_samp_${N}.recode_noinv.vcf
+bcftools view -c 1 temp/Clade_3_samp_${N}.recode.vcf -o vcf/Clade_3_samp_${N}.recode_noinv.vcf
 done
 
 #clean up 
 rm temp/*temp*
-rm vcf/*recode.vcf
+rm temp/*recode.vcf
