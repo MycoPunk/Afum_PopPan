417095f2012e7c6b744c6cc29bc5677306e035f5
Lotus Lofgren <LotusLofgren@gmail.com>
2021-01-14 15:01:01 -0800

    Update 03_create_summary.sh

--- pipelines/LD_decay/03_create_summary.sh
+++ pipelines/LD_decay/03_create_summary.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/bash
-#SBATCH --mem=64G -p batch --nodes 1 --ntasks 2 --out summary.log
+#SBATCH --mem=64G -p batch --nodes 1 --ntasks 2 --out logs/summary.log
 
 #Clade1
 for N in {1..50}; do \
-cat Clade_1_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > Clade_1_samp${N}_LD_out.ld.summary
+cat LD_out/Clade_1_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > LD_out/Clade_1_samp${N}_LD_out.ld.summary
 done
 
 #Clade2
 for N in {1..50}; do \
-cat Clade_2_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > Clade_2_samp${N}_LD_out.ld.summary
+cat LD_out/Clade_2_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > LD_out/Clade_2_samp${N}_LD_out.ld.summary
 done
 
 #Clade3
-cat Clade_3_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > Clade_3_LD_out.ld.summary
+for N in {1..50}; do \
+cat LD_out/Clade_3_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > LD_out/Clade_3_samp${N}_LD_out.ld.summary
+done
 
 #cleanup
 rm *ld
