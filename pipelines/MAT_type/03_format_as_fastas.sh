8f4191940f58596efea7384f327f5a8dcd92d92e
Lotus Lofgren <LotusLofgren@gmail.com>
2021-02-11 14:03:32 -0800

    Update 03_format_as_fastas.sh

--- pipelines/MAT_type/03_format_as_fastas.sh
+++ pipelines/MAT_type/03_format_as_fastas.sh
@@ -3,6 +3,7 @@
 
 module load ncbi-blast/2.9.0+
 
+mkdir results_fastas
 cd genomes
 
 ##run BLAST with seq output
@@ -21,18 +22,18 @@ done
 
 #run blast and output sequence hits
 for i in *.rename.fa; do
-blastn -query ../fastas/MAT1-2_Afu3g06170_nt  -db $i -max_hsps 1 -evalue .0001 -outfmt "6 sseqid sseq" -word_size 10 -out $i.MAT1-2.forfasta.out
+blastn -query ../fastas/MAT1-2_Afu3g06170_nt  -db $i -evalue .0001 -outfmt "6 sseqid sseq" -word_size 10 -out $i.MAT1-2.forfasta.out
 done
 
 for i in *.rename.fa; do
-blastn -query ../fastas/MAT1-1_Afum_CDSregion_Paoletti_nt  -db $i -max_hsps 1 -evalue .0001 -outfmt "6 sseqid sseq" -word_size 10 -out $i.MAT1-1.forfasta.out
+blastn -query ../fastas/MAT1-1_Afum_CDSregion_Paoletti_nt  -db $i -evalue .0001 -outfmt "6 sseqid sseq" -word_size 10 -out $i.MAT1-1.forfasta.out
 done
 
-mv *MAT1-2.forfasta.out ../results
-mv *MAT1-1.forfasta.out ../results
+mv *MAT1-2.forfasta.out ../results_fastas
+mv *MAT1-1.forfasta.out ../results_fastas
 
 ##format output files to fasta format
-cd ../results
+cd ../results_fastas
 #add carrot
 for i in *.forfasta.out; do
 awk '{ if ($0 ~ /_/) { printf ">"; } print $0; }' $i > $i.carrot.fa
@@ -41,22 +42,22 @@ done
 #add newline
 for i in *.scaffolds.fa.rename.fa.MAT1-1.forfasta.out.carrot.fa; do
 name=$(basename "$i" .scaffolds.fa.rename.fa.MAT1-1.forfasta.out.carrot.fa)
-sed 's/\t/\n/g' $i > $name.MAT1-1.fasta
+sed 's/\t/\n/g' $i > $name.MAT1-1.temp
 done
 
 for i in *.scaffolds.fa.rename.fa.MAT1-2.forfasta.out.carrot.fa; do
 name=$(basename "$i" .scaffolds.fa.rename.fa.MAT1-2.forfasta.out.carrot.fa)
-sed 's/\t/\n/g' $i > $name.MAT1-2.fasta
+sed 's/\t/\n/g' $i > $name.MAT1-2.temp
 done
 
-#add counter - note only necessary if you DON'T use -max_hsps 1 and want to make alignments for all hits
-#for i in *.MAT.temp; do
-#name=$(basename "$i" .MAT.temp)
-#awk '/^>/{$0=$0"-"(++i)}1' $i > $name.MAT.fasta
-#done
+#add counter
+for i in *.temp; do
+name=$(basename "$i" .temp)
+awk '/^>/{$0=$0"-"(++i)}1' $i > $name.fasta
+done
 
 #clean up
 rm *carrot.fa 
 rm *.forfasta.out
-#rm *MAT.temp
+rm *.temp
 #rm *fa.out
