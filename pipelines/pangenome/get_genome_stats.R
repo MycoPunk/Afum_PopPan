5c92205af4746254b897bd6685430cd777d5229c
Lotus Lofgren <LotusLofgren@gmail.com>
2021-11-11 15:12:35 -0500

    Update get_genome_stats.R

--- pipelines/pangenome/get_genome_stats.R
+++ pipelines/pangenome/get_genome_stats.R
@@ -1,19 +1,14 @@
 
-#get genome statitics 
+#get genome statistics 
 
 #run other genome stats quick
-genome_stats<-read.delim("genome_stats.csv", header = TRUE, sep = ",", fill = TRUE, strip.white = TRUE)
+genome_stats<-read.delim("data/genome_stats.csv", header = TRUE, sep = ",", fill = TRUE, strip.white = TRUE)
 names(genome_stats)
 
 #busco stats
 mean(genome_stats$BUSCO.score)
 max(genome_stats$BUSCO.score)
 min(genome_stats$BUSCO.score)
-
-#depth stats
-mean(genome_stats$sequence.depth..bp.)
-max(genome_stats$sequence.depth..bp.)
-min(genome_stats$sequence.depth..bp.)
 
 #depth stats
 mean(genome_stats$sequence.depth..bp.)
@@ -30,37 +25,37 @@ mean(genome_stats$N50)
 max(genome_stats$N50)
 min(genome_stats$N50)
 
-#N50 stats
-mean(genome_stats$N50)
-max(genome_stats$N50)
-min(genome_stats$N50)
-
 #assembily size stats
 mean(genome_stats$Assembly.size..bp.)
 max(genome_stats$Assembly.size..bp.)
 min(genome_stats$Assembly.size..bp.)
 
 #is there a relationship between genome size and the number of singleton / accessory genes?
-n_singletons_by_strain<-read.delim("n_singletons_by_strain.txt", header = TRUE, sep = "\t", fill = TRUE, strip.white = TRUE)
-n_accessory_by_strain<-read.delim("n_accessory_by_strain.txt", header = TRUE, sep = "\t", fill = TRUE, strip.white = TRUE)
+n_singletons_by_strain<-read.delim("data/n_singletons_by_strain_OF.txt", header = TRUE, sep = "\t", fill = TRUE, strip.white = TRUE)
+n_accessory_by_strain<-read.delim("data/n_accessory_by_strain_OF.txt", header = TRUE, sep = "\t", fill = TRUE, strip.white = TRUE)
 
 #fix names so that they match
 genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "-", replacement = "_"))
 n_singletons_by_strain<- data.frame(sapply(n_singletons_by_strain, gsub, pattern = "-", replacement = "_"))
 n_accessory_by_strain<- data.frame(sapply(n_accessory_by_strain, gsub, pattern = "-", replacement = "_"))
-genome_stats$strain.name<- paste("Afum", genome_stats$strain.name, sep ="_")
-genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afum_A_fum_", replacement = "Afum_"))
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "A_fum_", replacement = ""))
+
 #get stragglers 
 genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "DMC2_", replacement = ""))
 n_singletons_by_strain<- data.frame(sapply(n_singletons_by_strain, gsub, pattern = "DMC2_", replacement = ""))
 n_accessory_by_strain<- data.frame(sapply(n_accessory_by_strain, gsub, pattern = "DMC2_", replacement = ""))
-genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afum_Afu_343_P/11", replacement = "Afum_Afu_343_P_11"))
-genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afum_117535A_1.1", replacement = "Afum_117535A_1_1"))
-genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afum_B7594CDC_27", replacement = "Afum_B7594_CDC_27"))
-genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afum_AFIS1435CDC_6", replacement = "Afum_AFIS1435_CDC_6"))
-genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afum_B5258CDC_22", replacement = "Afum_B5258_CDC_22"))
-genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afum_B6894CDC_5", replacement = "Afum_B6894_CDC_5"))
-genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afum_B5960CDC_29", replacement = "Afum_B5960_CDC_29"))
+
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "AFIS1435CDC_6", replacement = "AFIS1435_CDC_6"))
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "EXP", replacement = "EP"))
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "Afu_343_P/11", replacement = "Afu_343_P_11"))
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "117535A_1.1", replacement = "117535A_1_1"))
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "B7594CDC_27", replacement = "B7594_CDC_27"))
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "B5258CDC_22", replacement = "B5258_CDC_22"))
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "B6894CDC_5", replacement = "B6894_CDC_5"))
+genome_stats<- data.frame(sapply(genome_stats, gsub, pattern = "B5960CDC_29", replacement = "B5960_CDC_29"))
+
+#check that names all match
+setdiff(n_singletons_by_strain$strain, genome_stats$strain.name)
 
 colnames(n_singletons_by_strain)<- c("n_singletons", "strain.name")
 colnames(n_accessory_by_strain)<- c("n_accessory", "strain.name")
@@ -94,7 +89,7 @@ to_print<- plot + facet_wrap(~group, scales = "free") +
   scale_fill_manual(values = c("#BA9141", "#316A6E"))+
   scale_color_manual(values=c("#BA9141", "#316A6E")) + geom_smooth(method=lm, fullrange=TRUE, se=FALSE)
   
-
+to_print
 
 ggsave("genome_size.pdf",to_print, width=8, height=4, units="in")
 
