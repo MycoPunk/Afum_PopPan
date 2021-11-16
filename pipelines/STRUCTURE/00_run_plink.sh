687810fe3f57d4ee4fc24791b6eac0ec281fa96d
Lotus Lofgren <LotusLofgren@gmail.com>
2021-11-16 11:29:47 -0500

    Update 00_run_plink.sh

--- pipelines/STRUCTURE/00_run_plink.sh
+++ pipelines/STRUCTURE/00_run_plink.sh
@@ -1,8 +1,7 @@
-
 #!/usr/bin/bash
 #SBATCH --mem 2gb
 
 #make the bed files that structure needs using plink
 
 module load plink
-plink --vcf vcf/262_strains.selected.SNP.NO_TEs.vcf.gz --const-fid --allow-extra-chr  --vcf-idspace-to _ --keep-allele-order --make-bed --out Afum_262
+plink --vcf vcf/Pop_for_pan_260.All.SNP.combined_selected.NO.TE.vcf.gz --const-fid --allow-extra-chr  --vcf-idspace-to _ --keep-allele-order --make-bed --out Afum_260
