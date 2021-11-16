2d73e860f99f006318bf17fcacbf249566fba504
Lotus Lofgren <LotusLofgren@gmail.com>
2021-11-16 11:47:56 -0500

    Update 01_run_structure_ARRAY.sh

--- pipelines/STRUCTURE/01_run_structure_ARRAY.sh
+++ pipelines/STRUCTURE/01_run_structure_ARRAY.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/bash
 #SBATCH --nodes 1 --ntasks 1 --mem 16gb --out structure.%a.log
 
+module unload miniconda2
 module load faststructure
 
 module list
-module unload python
-module unload miniconda3
-module load miniconda2
 
-#the numbers of K to investigate is equal to the number of array jobs requested (I ran K = 1-15)
+#the numbers of K to investigate is equal to the number of array jobs requested
 K=${SLURM_ARRAY_TASK_ID}
 
-#set N_REP equal to the number of iterations to run for each value of K.
+#set N_REP equal to the number of iterations to run for each value of K. 
 for N_REP in {1..30}; do \
 mkdir rep_${N_REP}
-structure.py -K $K --input=Afum_262 --output=rep_${N_REP}/Afum_262
+structure.py -K $K --input=Afum_260 --output=rep_${N_REP}/Afum_260
 done
