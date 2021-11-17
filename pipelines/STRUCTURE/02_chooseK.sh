1cd359e75e78b0cf382eae3917e8fb4b50f81552
Lotus Lofgren <LotusLofgren@gmail.com>
2021-11-16 20:27:33 -0500

    Update 02_chooseK.sh

--- pipelines/STRUCTURE/02_chooseK.sh
+++ pipelines/STRUCTURE/02_chooseK.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
+#SBATCH --nodes 1 --ntasks 1 --mem 16gb --out chooseK.%a.log
+
+module unload miniconda2
 module load faststructure
 
 for N_REP in {1..30}; do \
-chooseK.py --input=rep_${N_REP}/Afum_262
+chooseK.py --input=rep_${N_REP}/Afum_260
 done
