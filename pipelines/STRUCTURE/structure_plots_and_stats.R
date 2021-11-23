300dcde7c6d57d6d54a3bd9ee6d3b7717ce0c733
Lotus Lofgren <LotusLofgren@gmail.com>
2021-11-23 11:38:33 -0500

    Update structure_plots_and_stats.R

--- pipelines/STRUCTURE/structure_plots_and_stats.R
+++ pipelines/STRUCTURE/structure_plots_and_stats.R
@@ -5,11 +5,12 @@ library(grid)
 library(gridExtra)
 library(svglite)
 library(tidyr)
+library(multcomp)
 
 #setwd("")
 
 #first get the group assignments
-inds <- read.table("data/strain_names_in_order.csv",header=FALSE,stringsAsFactors=F)
+inds <- read.table("strain_names_in_order.csv",header=FALSE,stringsAsFactors=F)
 inds$V1
 
 ffiles <- list.files(path=".",pattern="*.meanQ",full.names=T)
@@ -20,7 +21,7 @@ length(flist[[1]])
 rownames(flist[[1]]) <- (inds$V1)
 if(length(unique(sapply(flist,nrow)))==1) flist <- lapply(flist,"rownames<-",inds$V1)
 # show row names of all runs and all samples
-#lapply(flist, rownames)
+lapply(flist, rownames)
 
 # view head of first converted file
 head(flist[[1]])
@@ -36,39 +37,40 @@ tabulateQ(flist, writetable=TRUE, sorttable=TRUE)
 sr1 <- summariseQ(tr1)
 summariseQ(tr1, writetable=TRUE)
 
+
 #plot
-p1 <- plotQ(alignK(sortQ(flist)[1:4]),imgoutput="join",returnplot=T,exportplot=F,quiet=T,basesize=11,
+p1 <- plotQ(alignK(sortQ(flist)[1:5]),imgoutput="join",returnplot=T,exportplot=F,quiet=T,basesize=11,
             clustercol=c("#56326E",
                          "#ED7F6F",
                          "#ABA778",
                          "#F2CC85", 
                          "#D4494E",
-                         "#7D7A70"),
-            showlegend=T, legendlab=c("Clade 1","Clade 2","Clade 3","Clade 4","Clade 5"),
+                         "#7D7A70", 
+                         "#7E7A70"),
+            showlegend=T, legendlab=c("Clade 1","Clade 2","Clade 3","Clade 4","Clade 5", "Clade 6"),
             legendkeysize=10,legendtextsize=10,legendmargin=c(2,2,2,0),
             showindlab=F,useindlab=T,showyaxis=T, indlabsize = 4,
             sortind="all", sharedindlab = F, panelspacer=0.4,
             barbordersize=1)
 grid.arrange(p1$plot[[1]])
 
-
-
 #my_cols_me<-c("#56326E","#ED7F6F","#ABA778","#F2CC85", "#D4494E","#7D7A70")
 #scales::show_col(my_cols_me); my_cols_me
 
+#get K=3
+flist2<- as.data.frame(flist[2])
 
-#get likely admixed individuals 
-
-admixed<- flist2[apply(flist2[1:3], 1, function(x) all(x < .75)), ] 
+admixed<- flist2[apply(flist2[1:3], 1, function(x) all(x < .80)), ] 
+admixed
 #here there are 6 of them 
 
-#Afum_266.3.Cluster1 Afum_266.3.Cluster2 Afum_266.3.Cluster3
-#AF293             0.605493            0.372659            0.021848
-#CF098             0.605721            0.372531            0.021748
-#CM7632            0.621613            0.345604            0.032783
-#F18304            0.599702            0.378497            0.021801
-#F7763             0.699989            0.146177            0.153834
-#RSF2S8            0.363823            0.636174            0.000004
+#Afum_260.3.Cluster1 Afum_260.3.Cluster2 Afum_260.3.Cluster3
+#AF293             0.591927            0.408069            0.000003
+#CF098             0.592183            0.407814            0.000003
+#CM7632            0.608942            0.371715            0.019343
+#F18304            0.586671            0.413325            0.000003
+#F7763             0.682733            0.172096            0.145171
+#RSF2S8            0.367427            0.632569            0.000004
 
 
 #plot admixed individuals only
@@ -84,7 +86,8 @@ p1 <- plotQ(alignK(sortQ(only_admixed)[2]),returnplot=T,exportplot=F,quiet=T,bas
                          "#ABA778",
                          "#F2CC85", 
                          "#D4494E",
-                         "#7D7A70"),
+                         "#7D7A70", 
+                         "#7E7A70"),
             showlegend=T, legendlab=c("Clade 1","Clade 2","Clade 3"),
             legendkeysize=10,legendtextsize=10,legendmargin=c(2,2,2,0),
             showindlab=T,useindlab=T,showyaxis=T, indlabsize = 10,
@@ -94,38 +97,70 @@ grid.arrange(p1$plot[[1]])
 
 
 #make plots from chooseK.py
-choose_k <- read.csv("data/choose_k_results.csv",header=FALSE, row.names = 1, stringsAsFactors=F)
-choose_k_t<- data.frame(t(choose_k))
-
+choose_k <- read.csv("choose_k_results.csv",header=FALSE, row.names = 1, stringsAsFactors=F, check.names = F)
+choose_k_sub<- choose_k[,4:ncol(choose_k)]
+#transpose
+choose_k_t<- data.frame(t(choose_k_sub))
 
 #make long
-choose_k_long <- choose_k_t %>% pivot_longer(cols = 2:30, names_to = "Run_n", values_to = "ML")
+choose_k_long <- choose_k_t %>% pivot_longer(cols = 2:31, names_to = "Run_n", values_to = "ML")
+
+choose_k_long$Run_n<- gsub("X", "", choose_k_long$Run_n)
+choose_k_long$itteration<- gsub("ML K =", "", choose_k_long$itteration)
 
 #plot
-p<- ggplot(data = choose_k_long, mapping = aes(x = itteration, y = ML, group = Run_n, color = Run_n)) + 
+p<- ggplot(data = choose_k_long, mapping = aes(x = as.numeric(itteration), y = as.numeric(ML), group = Run_n, color = Run_n)) + 
   geom_line(color="#D4494E", size=1, alpha=.2, linetype=1) +
   theme_minimal() +
   xlab("K") + ylab("Marginal Likelihood")+
   scale_x_discrete(limits=c("1","2","3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"))
 p
-ggsave(plot = p, width = 6, height = 2, dpi = 300, filename = "Marginal_likelyhood.svg")
+#ggsave(plot = p, width = 6, height = 2, dpi = 300, filename = "Marginal_likelyhood.pdf")
 
 
-##Run stats on what point is no longer a significant increase in K 
+#get max cluster assignments for K=3
+#rename
+colnames(flist2)<- c("1", "2", "3")
+flist2$clade_assignment<- colnames(flist2)[max.col(flist2,ties.method="first")]
+
+#get n in each clade 
+table(flist2$clade_assignment)
+#Clade 1 Clade 2 Clade 3 
+#200      45      15 
+#which is the same n assignments per clade as DAPCA 
+
+#format for printing to match the DAPCA sup table. 
+colnames(flist2)<- c("prob_clade1", "prob_clade1", "prob_clade1", "clade_assignment")
+#reorder to last col is first
+print_df<- flist2[,c(ncol(flist2),1:(ncol(flist2)-1))]
+print_df$strain<- row.names(print_df)
+print_df<- print_df[,c(ncol(print_df),1:(ncol(print_df)-1))]
+
+#write posteriors with assignments for supplemental table
+#write.table(print_df, "posteriors_fastSTRUCTURE.txt", sep="\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+
+###Run stats on what point is no longer a significant increase in K 
 #create differences df - a dataframe of differences between that col n (k) and col n - 1 (k - 1)
-K_diffs<-  data.frame(t(apply(choose_k,1,diff)))
-K_diffs_clean<- K_diffs[-1,]
+choose_k_sub_input<- choose_k_sub[2:nrow(choose_k_sub),]
+str(choose_k_sub_input)
+
+#make numeric
+choose_k_sub_input<-as.data.frame(lapply(choose_k_sub_input,as.numeric))
+class(choose_k_sub_input$V5)
+
+K_diffs<-  data.frame(t(apply(choose_k_sub_input,1,diff)))
+
+#K_diffs_clean<- K_diffs[,-1]
 
 #rename confusing col headers
-colnames(K_diffs_clean)<- seq(1:14)
+colnames(K_diffs)<- seq(1:14)
 
 #make long
-choose_k_long_diffs <- K_diffs_clean %>% pivot_longer(cols = 1:14, names_to = "K", values_to = "ML")
-
+choose_k_long_diffs <- K_diffs %>% pivot_longer(cols = 1:14, names_to = "K", values_to = "ML")
 
 #run anova
-choose_k_long
-aov(K_diffs_clean)
+#aov(K_diffs_clean)
 
 #change to factor
 choose_k_long_diffs$K = as.factor(choose_k_long_diffs$K)
@@ -135,7 +170,6 @@ k_diffs_anova<- anova(k_diffs_lm)
 summary(k_diffs_anova)
 
 #posthoc test
-library(multcomp)
 # running glht()
 post.hoc <- glht(k_diffs_lm, linfct = mcp(K = 'Tukey'))
 
@@ -144,8 +178,8 @@ summary(post.hoc, test = adjusted("bonferroni"))
 #does not increase significantly after k = 4
 
 #difference between K vals
-mean(K_diffs_clean[,1])
-mean(K_diffs_clean[,2])
-mean(K_diffs_clean[,3])
-mean(K_diffs_clean[,4])
+abs(mean(K_diffs_clean[,1])) #mean difference between K1 and K2
+abs(mean(K_diffs_clean[,2])) #mean difference between K2 and K3
+abs(mean(K_diffs_clean[,3])) #mean difference between K3 and K4
+abs(mean(K_diffs_clean[,4])) #mean difference between K4 and K5
 
