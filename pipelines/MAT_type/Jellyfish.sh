be61e2db002a0a9ff1c70c66b36df59c7e96c35e
Lotus Lofgren <LotusLofgren@gmail.com>
2022-01-10 21:23:58 -0500

    add two strains

--- pipelines/MAT_type/Jellyfish.sh
+++ pipelines/MAT_type/Jellyfish.sh
@@ -3,7 +3,7 @@
 
 module load jellyfish
 
-#jellyfish count -C -m 21 -s 1000000000 -t 10 genomes/Aspergillus_fumigatus_Afu_343_P_11.scaffolds.fa -o reads.jf
+jellyfish count -C -m 21 -s 1000000000 -t 10 genomes/Aspergillus_fumigatus_Afu_343_P_11.scaffolds.fa -o reads.jf
 jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/IFM_61407_R1.test.fq  -o IFM_61407_reads.jf
 jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/IFM_59359_R1.test.fq  -o IFM_59359_reads.jf
 jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/DMC2_AF100-1_3_R1.test.fq  -o DMC2_AF100-1_3_reads.jf
@@ -13,6 +13,8 @@ jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/NCPF-7816_R1.test.fq  -o
 jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/Afu_343-P-11_R1.test.fq  -o Afu_343-P-11_reads.jf
 jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/B7586_CDC-30_R1.test.fq  -o B7586_CDC-30_reads.jf
 jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/AF100-12_5_R1.test.fq  -o AF100-12_5_reads.jf
+jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/SF2S9_R1.fastq  -o SF2S9_reads.jf
+jellyfish count -C -m 21 -s 1000000000 -t 10 raw_fastas/AF100-12_7G_R1.fastq  -o AF100-12_7G_reads.jf
 
 
 
@@ -26,3 +28,5 @@ jellyfish histo -t 10 NCPF-7816_reads.jf > NCPF-7816_reads.histo
 jellyfish histo -t 10 Afu_343-P-11_reads.jf > Afu_343-P-11_reads.histo
 jellyfish histo -t 10 B7586_CDC-30_reads.jf > B7586_CDC-30_reads.histo
 jellyfish histo -t 10 AF100-12_5_reads.jf > AF100-12_5_reads.histo
+jellyfish histo -t 10 SF2S9_reads.jf > SF2S9_reads.histo
+jellyfish histo -t 10 AF100-12_7G_reads.jf > AF100-12_7G_reads.histo
