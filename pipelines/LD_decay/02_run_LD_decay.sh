adb7411767475a8a6086e900a28aa812f118ce5e
Lotus Lofgren <LotusLofgren@gmail.com>
2022-01-19 22:24:30 -0500

    Update 02_run_LD_decay.sh

--- pipelines/LD_decay/02_run_LD_decay.sh
+++ pipelines/LD_decay/02_run_LD_decay.sh
@@ -14,18 +14,23 @@ fi
 mkdir LD_out
 
 #Clade1
-for N in {1..50}; do \
+for N in {1..20}; do \
 plink --bfile "plink_files/Clade_1_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 99999 --ld-window-kb 500 --threads $CPUS --out "LD_out/Clade_1_samp${N}_LD_out"
 done
 
 #Clade2
-for N in {1..50}; do \
+for N in {1..20}; do \
 plink --bfile "plink_files/Clade_2_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 99999 --ld-window-kb 500 --threads $CPUS --out "LD_out/Clade_2_samp${N}_LD_out"
 done
 
 #Clade3
-for N in {1..50}; do \
+for N in {1..20}; do \
 plink --bfile "plink_files/Clade_3_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 99999 --ld-window-kb 500 --threads $CPUS --out "LD_out/Clade_3_samp${N}_LD_out"
+done
+
+#All
+for N in {1..20}; do \
+plink --bfile "plink_files/n_12_samp${N}" --r2 --allow-extra-chr --ld-window-r2 0 --ld-window 99999 --ld-window-kb 500 --threads $CPUS --out "LD_out/n_12_samp${N}_LD_out"
 done
 
 #clean up 
