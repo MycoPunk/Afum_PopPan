e41084d66c589f28b3ef87cf160cf22a4bbb98ac
Lotus Lofgren <LotusLofgren@gmail.com>
2022-01-19 22:27:23 -0500

    Update 03_create_summary.sh

--- pipelines/LD_decay/03_create_summary.sh
+++ pipelines/LD_decay/03_create_summary.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/bash
-#SBATCH --mem=64G -p batch --nodes 1 --ntasks 2 --out logs/summary.log
+#SBATCH --mem=85G -p batch --nodes 1 --ntasks 2 --out logs/summary.log
 
 #Clade1
-for N in {1..50}; do \
+for N in {1..20}; do \
 cat LD_out/Clade_1_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > LD_out/Clade_1_samp${N}_LD_out.ld.summary
 done
 
 #Clade2
-for N in {1..50}; do \
+for N in {1..20}; do \
 cat LD_out/Clade_2_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > LD_out/Clade_2_samp${N}_LD_out.ld.summary
 done
 
 #Clade3
-for N in {1..50}; do \
+for N in {1..20}; do \
 cat LD_out/Clade_3_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > LD_out/Clade_3_samp${N}_LD_out.ld.summary
+done
+
+#All
+for N in {1..20}; do \
+cat LD_out/n_12_samp${N}_LD_out.ld | sed 1,1d | awk -F " " 'function abs(v) {return v < 0 ? -v : v}BEGIN{OFS="\t"}{print abs($5-$2),$7}' | sort -k1,1n > LD_out/n_12_samp${N}_LD_out.ld.summary
 done
 
 #cleanup
