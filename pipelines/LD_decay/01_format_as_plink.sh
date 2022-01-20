22b434172c753c8502e120ea9a0bc2d0f18926d9
Lotus Lofgren <LotusLofgren@gmail.com>
2022-01-19 22:19:56 -0500

    Update 01_format_as_plink.sh

--- pipelines/LD_decay/01_format_as_plink.sh
+++ pipelines/LD_decay/01_format_as_plink.sh
@@ -7,16 +7,21 @@ module load plink
 mkdir plink_files
 
 #Clade1
-for N in {1..50}; do \
+for N in {1..20}; do \
 plink --vcf vcf/Clade_1_samp_${N}.recode_noinv.vcf --const-fid --allow-extra-chr  --vcf-idspace-to _ --keep-allele-order --make-bed --out plink_files/Clade_1_samp${N}
 done
 
 #Clade2
-for N in {1..50}; do \
+for N in {1..20}; do \
 plink --vcf vcf/Clade_2_samp_${N}.recode_noinv.vcf --const-fid --allow-extra-chr  --vcf-idspace-to _ --keep-allele-order --make-bed --out plink_files/Clade_2_samp${N}
 done
 
 #Clade3
-for N in {1..50}; do \
+for N in {1..20}; do \
 plink --vcf vcf/Clade_3_samp_${N}.recode_noinv.vcf --const-fid --allow-extra-chr  --vcf-idspace-to _ --keep-allele-order --make-bed --out plink_files/Clade_3_samp${N}
+done
+
+#All
+for N in {1..20}; do \
+plink --vcf vcf/n_12_samp_${N}.recode_noinv.vcf --const-fid --allow-extra-chr  --vcf-idspace-to _ --keep-allele-order --make-bed --out plink_files/n_12_samp${N}
 done
