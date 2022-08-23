56c3f2a2fb661119a5d62fa05de47ec4515302c8
Lotus Lofgren <LotusLofgren@gmail.com>
2022-08-23 10:19:56 -0400

    Create plot_treemic.R

---
+++ pipelines/TreeMix/plot_treemic.R
@@ -0,0 +1,11 @@
+#This script generates a TreeMix plot for the A.fumigatus pan genome dataset
+
+#######
+
+sessionInfo()
+#set wd
+setwd("~/Desktop/Project_Afum_pangenome_3/TreeMix_w_OG_optimized")
+
+#plot
+source("~/Desktop/R/treemix-1.13/src/plotting_funcs.R")
+plot_tree(stem="TreeMix")
